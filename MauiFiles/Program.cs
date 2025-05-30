e15282c77bcd89a4c159f8d363e6b6195e2c077b
Andy Lech <andylech@users.noreply.github.com>
2025-05-29 20:38:58 -0400

    Add CleanBlog as downloaded theme
    Uses Devlead.Statiq NuGet package.
    Details here: https://www.devlead.se/posts/2021/2021-04-10-devlead-statiq-part2-theme-from-uri

--- MauiFiles/Program.cs
+++ MauiFiles/Program.cs
@@ -1,5 +1,9 @@
 ﻿
+const string cleanBlogThemeZipFile = "https://github.com/statiqdev/CleanBlog/archive/ad639891c4469a2c9fc1d16b0ef8f99e975e82d8.zip";
+
 return await Bootstrapper
-  .Factory
-  .CreateWeb(args)
-  .RunAsync();
+    .Factory
+    .CreateDefault(args)
+    .AddThemeFromUri(new Uri(cleanBlogThemeZipFile))
+    .AddWeb()
+    .RunAsync();
